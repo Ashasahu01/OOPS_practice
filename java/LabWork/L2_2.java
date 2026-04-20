54839edc1815b93c59ceceb43851bdc54f23b0d9
Ashasahu01 <as20252651013@gmail.com>
2026-04-20 23:54:52 +0530

    java practice code

--- java/LabWork/L2_2.java
+++ java/LabWork/L2_2.java
@@ -1,35 +1,10 @@
 package LabWork;
 
+import java.util.ArrayList;
 public class L2_2 {
-public static int[] removeDuplicates(int[] arr) {
-
-    int n = arr.length;
-    int[] temp = new int[n];   // temporary array
-    int k = 0;               
-
-    for (int i = 0; i < n; i++) {
-
-        boolean isDuplicate = false;
-
-        for (int j = 0; j < k; j++) {
-            if (arr[i] == temp[j]) {
-                isDuplicate = true;
-                break;
-            }
-        }
-
-        if (!isDuplicate) {
-            temp[k] = arr[i];
-            // System.out.print(temp[k]);
-            k++;
-        }
+  class StackUnderflowException extends RuntimeException {
+    public StackUnderflowException(String message) {
+        super(message);
     }
-
-    int[] result = new int[k];
-    for (int i = 0; i < k; i++) {
-        result[i] = temp[i];
-    }
-
-    return result;
-}
+    }  
 }
---
+++ java/LabWork/L2_5.java
@@ -0,0 +1,35 @@
+package LabWork;
+
+public class L2_5 {
+public static int[] removeDuplicates(int[] arr) {
+
+    int n = arr.length;
+    int[] temp = new int[n];   // temporary array
+    int k = 0;               
+
+    for (int i = 0; i < n; i++) {
+
+        boolean isDuplicate = false;
+
+        for (int j = 0; j < k; j++) {
+            if (arr[i] == temp[j]) {
+                isDuplicate = true;
+                break;
+            }
+        }
+
+        if (!isDuplicate) {
+            temp[k] = arr[i];
+            // System.out.print(temp[k]);
+            k++;
+        }
+    }
+
+    int[] result = new int[k];
+    for (int i = 0; i < k; i++) {
+        result[i] = temp[i];
+    }
+
+    return result;
+}
+}
